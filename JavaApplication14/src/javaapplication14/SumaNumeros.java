3224e21f5bc0391562c5474bb0a723ec8ea243d6
anthony <anthony@DESKTOP-G6N9O9E>
2017-10-18 13:43:31 +0200

    1.0

---
+++ JavaApplication14/src/javaapplication14/SumaNumeros.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication14;
+
+
+public class SumaNumeros {
+    
+    public static void main(String[] args) {
+       //sumando1
+       int a1=10;
+       
+       //sumando2
+       int a2=6;
+       
+       //suma
+       int suma;
+       
+       suma=a1+a2;
+       
+        System.out.println("El resultado es: "+suma);
+    }
+}
